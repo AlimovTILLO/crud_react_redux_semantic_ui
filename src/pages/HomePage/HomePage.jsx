fb91ed3ab3fd241f27cb559d13c493e9fd0a69d7
Alimov Tillo <alimovtillo@gmail.com>
2020-01-28 14:31:08 +0600

    add filter  method to products

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,61 +1,89 @@
 import React from 'react'
 import _ from 'lodash'
-import { Container, Pagination, Table, Header } from 'semantic-ui-react'
+import { Container, Pagination, Table, Header, Input, Select, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 import { productActions } from '../../actions';
 import { ResponsiveContainer } from '../../components/ResponsiveContainer'
 import PropTypes from 'prop-types'
 
-
-
 ResponsiveContainer.propTypes = {
     children: PropTypes.node,
 }
+
+const options = [
+    { key: 'name', text: 'Name', value: 'name' },
+    { key: 'year', text: 'Year', value: 'year' },
+]
 
 class HomePage extends React.Component {
 
     state = {
         column: null,
         direction: null,
+        isLoading: false,
+        data: {},
+        search: "",
+        searchValue: "",
+        selectValue: "name"
     }
-
 
     componentDidMount() {
         this.props.getProducts({ page: 1, per_page: 4 });
     }
 
+    handleInputChange = (e, { value }) => {
+        this.setState({ search: value })
+    }
+
+    handleSearchChange = () => {
+        this.setState({ searchValue: this.state.search })
+    }
+
+    handleSelectChange  = (e, { value }) => {
+        this.setState({ selectValue: value });
+    }
+
     handleSort = (clickedColumn) => () => {
-        const data = this.props.products.pages.data
+
+        const source = this.props.products.pages.data
         const { column, direction } = this.state
 
         if (column !== clickedColumn) {
             this.setState({
                 column: clickedColumn,
-                data: _.sortBy(data, [clickedColumn]),
+                data: _.sortBy(source, [clickedColumn]),
                 direction: 'ascending',
             })
-
             return
         }
 
         this.setState({
-            data: data.reverse(),
+            data: source.reverse(),
             direction: direction === 'ascending' ? 'descending' : 'ascending',
         })
     }
 
 
     handlePaginationChange = (e, { activePage }) => {
+        this.setState({ searchValue: '' })
         this.props.getProducts({ page: activePage, per_page: 4 });
     }
 
     render() {
-        const { column, direction } = this.state
+
+        const { column, direction, searchValue, selectValue } = this.state
         const { products } = this.props;
+        const { data: list = [] } = products.pages || {};
+        const filteredList = list.length ? list.filter(obj => obj[selectValue].toString().toLowerCase().startsWith(searchValue.toLowerCase())) : []
         return (
             <ResponsiveContainer>
                 <Container style={{ margin: '3em 0em 0em', padding: '3em 0em', minHeight: 'calc(100vh - 150px)' }}>
 
+                    <Input type='text' placeholder='Search...' action onChange={this.handleInputChange}>
+                        <input />
+                        <Select compact options={options} value={selectValue} onChange={this.handleSelectChange}/>
+                        <Button type='submit' onClick={this.handleSearchChange}>Search</Button>
+                    </Input>
                     <Header>Products:</Header>
 
                     <Table sortable celled fixed>
@@ -73,22 +101,22 @@ class HomePage extends React.Component {
                                     sorted={column === 'color' ? direction : null}
                                     onClick={this.handleSort('color')} >
                                     Color </Table.HeaderCell>
-                                <Table.HeaderCell
-                                    sorted={column === 'pantone_value' ? direction : null}
-                                    onClick={this.handleSort('pantone_value')} >
+                                <Table.HeaderCell>
                                     Pantone value </Table.HeaderCell>
                             </Table.Row>
                         </Table.Header>
 
                         <Table.Body>
-                            {products.pages && products.pages.data.map((user, index) =>
-                                <Table.Row key={user.id}>
-                                    <Table.Cell>{user.name}</Table.Cell>
-                                    <Table.Cell>{user.year}</Table.Cell>
-                                    <Table.Cell>{user.color}</Table.Cell>
-                                    <Table.Cell>{user.pantone_value}</Table.Cell>
-                                </Table.Row>
-                            )}
+                            {
+                                filteredList.map(({ id, name, year, color, pantone_value }) =>
+                                    // _.map(data, ({ id, name, year, color, pantone_value }) =>
+                                    <Table.Row key={id}>
+                                        <Table.Cell>{name}</Table.Cell>
+                                        <Table.Cell>{year}</Table.Cell>
+                                        <Table.Cell>{color}  <div style={{width: '20px', height: '20px', backgroundColor: color, display: 'inline-block'}}>&nbsp;</div></Table.Cell>
+                                        <Table.Cell>{pantone_value}</Table.Cell>
+                                    </Table.Row>
+                                )}
                         </Table.Body>
 
                         <Table.Footer>
@@ -111,12 +139,6 @@ class HomePage extends React.Component {
                             </Table.Row>
                         </Table.Footer>
                     </Table>
-
-
-
-
-
-
 
                 </Container>
             </ResponsiveContainer>
