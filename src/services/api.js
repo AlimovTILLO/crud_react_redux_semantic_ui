e4048123f4841bdb390bbcce20f469e9508e5d10
Alimov Tillo <alimovtillo@gmail.com>
2020-02-03 13:40:18 +0600

    add new api server

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-export const baseURL = 'http://127.0.0.1:8000/'
+export const baseURL = 'http://search.intuit.kg/'
 export const HTTP = axios.create({
   baseURL: baseURL
 })
